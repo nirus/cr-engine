8e2a4be608e8d9376b40316ab8e99c768fe3af82
Niranjan Kumar <nirus@live.in>
2022-09-16 15:28:37 +0200

    Developement/beta v1 (#1)
    * image load bug fix during buildtime
    
    * feature: mdmaker
    
    * stable v0.1
    
    * fixes the claim json parsing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
+import image from "@astrojs/image";
+import { codeRocksMiddleWare } from './plugin/crMiddleware.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [sitemap()],
-    site: "https://coder.rocks",
+  integrations: [sitemap(), image()],
+  site: "https://coder.rocks",
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [codeRocksMiddleWare]
+  }
 });
