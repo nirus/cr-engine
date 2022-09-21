f39c1d929ff89158885ea46fdac2d0ed4dd08667
nirus <nirus@live.in>
2022-09-22 01:32:45 +0200

    chaged the Symlink logic

--- scripts/symlink_publish/index.js
+++ scripts/symlink_publish/index.js
@@ -1,4 +1,5 @@
-const { symlinkSync } = require('fs');
+const { symlinkSync, writeFileSync, readFileSync, rm, rmSync } = require('fs');
 const { resolve } = require('path')
 
+rmSync('../../src/pages/posts/README.md', { force: true });
 symlinkSync(resolve('../../src/astro-code-pub/[...page].astro'), resolve('../../src/pages/posts/[...page].astro'));
