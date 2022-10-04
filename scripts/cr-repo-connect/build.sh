9d6ccf7e8b041ca17fb7a7576ee514f9f35580df
nirus <nirus@live.in>
2022-10-04 16:43:23 +0200

    * corrected the search indexer * fixed the build.sh error * added scripts to package.json

--- scripts/cr-repo-connect/build.sh
+++ scripts/cr-repo-connect/build.sh
@@ -10,7 +10,7 @@ rm -rf src/pages/posts/.git/info/sparse-checkout
 
 touch src/pages/posts/.git/info/sparse-checkout
 
-echo -e "/* \n!README.md" >> src/pages/posts/.git/info/sparse-checkout
+echo "/* \n!README.md" >> src/pages/posts/.git/info/sparse-checkout
 
 if [[ -n "$branch" ]]; then
     git --git-dir=src/pages/posts/.git --work-tree=src/pages/posts checkout $branch
