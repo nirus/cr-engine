8339d568cf6c7680dc78b1a6e922783e09d5d546
Niranjan Kumar <nirus@live.in>
2025-07-06 00:00:23 +0200

    feat: TECH change the tag line and small adjustment (#22)

---
+++ src/config/site.ts
@@ -0,0 +1,35 @@
+export const Settings = {
+  site: {
+    title: 'CoderRocks',
+    tagline: 'Code Meets Creativity',
+    gaTagId: '',
+    mainUrl: 'https://coder.rocks',
+    secondUrl: 'https://CoderRocks.com',
+    description:
+      'Coder Rocks is a blog site to share coding knowledge, ideas & learning through articles',
+  },
+
+  post: {
+    contentWidth: 'max-w-12xl',
+    avatarSize: '100',
+  },
+
+  indexPage: {
+    card: {
+      height: 192,
+      width: 360,
+    },
+  },
+
+  cr: {
+    email: 'mailtto:support@coder.rocks',
+    twitter: 'https://twitter.com/coder_rocks',
+    github: 'https://github.com/nirus/coder-rocks',
+  },
+
+  legendImageSize: {
+    width: 1024,
+    height: 420,
+    aspectRatio: 16 / 9,
+  },
+}
