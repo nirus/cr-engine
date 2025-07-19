c2e118266a70d67d8ea85b7cad1c2dbc345b91e3
nirus <nirus@live.in>
2025-07-20 00:47:49 +0200

    fix: TECH output server and yarnlock

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: cloudflare(),
   image: {
     service: passthroughImageService(),
