767d2c86ee15387bc8257b28eb0809f9f0813b30
nirus <nirus@live.in>
2025-07-20 13:35:12 +0200

    exclude preiview route

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   adapter: cloudflare({
     imageService: 'passthrough',
+    routes: {
+      exclude: ['/preview'],
+    },
   }),
   image: {
     service: passthroughImageService(),
