0cdb2ace0aa39a0ef94f4908df06c778b8e9f508
nirus <nirus@live.in>
2025-07-20 12:15:43 +0200

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   image: {
     service: passthroughImageService(),
   },
